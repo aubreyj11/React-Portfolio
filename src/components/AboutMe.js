2a04808b5e0a72b0f395143ba2be0ff8edfa619e
Aubrey Johnston <aubreyjohnston@Aubreys-MacBook-Air.local>
2023-05-19 16:50:47 -0700

    added about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import Profile from './images/ProfilePic.png'
 
 function AboutMe() {
   return (
-    <div>AboutMe</div>
+    <div>
+        <h2 className='aboutMeTitle'>Aubrey Johnston</h2>
+        <div className='aboutContainer'>
+            <p className='aboutMeText'> _ <br></br><br></br>I am a full stack web developer 
+            with a background in education located in Orange County, CA. I have a passion for learning and problem solving. 
+            I am a team player and I am excited to work with others to create amazing 
+            applications!<br></br><br></br> _</p>
+            <p className="aboutMeText2">_<br></br> <br></br>My name is Aubrey Johnston and I am a 21 year old coding student!<br></br> _</p>
+            <img className='profilePic' src={Profile} alt='Profile Img'/>
+        </div>
+    </div>
   )
 }
 
